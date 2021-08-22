e8069e474716e4462c8ee17ad6c5be4c58012385
BA_CalderonMorales <tbcm640@gmail.com>
2021-08-22 12:39:11 -0500

    Set up portfolio to soon include capability to animate SVG images/text in future iterations.

---
+++ script.js
@@ -0,0 +1,25 @@
+// var inputOne = "Is coding boring?"
+// var inputTwo = "Nope. It's mesmerizing."
+
+// var drawingOne = SVG().addTo('#drawingOne').viewbox(0, 0, 100, 100).preserveAspectRatio("xMidYMin slice");
+// var drawingTwo = SVG().addTo('#drawingTwo').viewbox(0, 0, 100, 100)
+
+
+// var textOne = drawingOne.text(function(add) {
+// 	add.tspan( inputOne )
+// })
+
+// var textTwo = drawingTwo.text(function(add) {
+// 	add.tspan( inputTwo )
+// })
+
+// textPathOne = textOne.path('M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80')
+// textPathTwo = textTwo.path('M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80')
+
+// textPathOne.animate(4000).ease('<>')
+// 	.plot('M10 80 C 40 150, 65 150, 95 80 S 150 10, 180 80')
+// 	.loop(true, true)
+// textPathTwo.animate(4000).ease('<>')
+// 	.plot('M10 80 C 40 150, 65 150, 95 80 S 150 10, 180 80')
+// 	.loop(true, true)
+
